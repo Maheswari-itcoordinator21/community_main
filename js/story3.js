1674e677130d7592adf7b709a9d84916c0c750ea
MAHI1119 <mahijanu1119@gmail.com>
2022-02-22 21:52:27 +0530

    storypage3

--- js/story3.js
+++ js/story3.js
@@ -22,22 +22,5 @@ const urlParams = new URLSearchParams(window.location.search);
                document.getElementById("mentee_img1").src = image.image;
            }
         })
-        // story .forEach ((linked)=> {
-        //     if (linked.id == id){
-        //         console.log(linked);
-        //         document.getElementById("mentee_link1").src= linked.linked;
-        //     }
-        //  })
-        // story .forEach ((design)=> {
-        //     if(design.id ==id){
-        //         console.log(design);
-        //         document.getElementById("mentee_dest1").innerHTML +=design.design;
-        //     }
-        // })
-        // story .forEach ((hi)=> {
-        //     if(hi.id ==id){
-        //         console.log(hi);
-        //         document.getElementById("mentee_hi1").innerHTML +=hi.hi;
-        //     }
-        // })
+        
        })
